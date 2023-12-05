ddd66e857ad4bc57be17f538ffeaea8d5147dcbb
Tony <antony.branquinho@gmail.com>
2023-12-05 11:52:15 -0300

    Facil

---
+++ Tabuada/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada {
+    class Program {
+        static void Main(string[] args) {
+
+            int n, i, resultado;
+
+            Console.Write("Deseja a tabuada para qual valor? ");
+            n = int.Parse(Console.ReadLine());
+
+            for (i = 1; i <= 10; i++) {
+                resultado = n * i;
+                Console.WriteLine(n + " x " + i + " = " + resultado);
+            }
+        }
+    }
+}
